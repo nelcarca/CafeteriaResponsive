54b620b1c4cb0a53157ff50d27c4cdf2cc99b245
Nelson Carvajal <nelcarca@gmail.com>
2021-12-10 23:06:23 -0300

    cambiosUltimos

--- js/menu.js
+++ js/menu.js
@@ -4,5 +4,6 @@ let menu = document.getElementById('menu');
 // burguerButton contiene el span con el icono
 // burguerButton.addEventListener('que evento', 'que deseo hacer');
 burgerButton.addEventListener('touchstart', function() {
+    console.log('hola')
   menu.classList.toggle('active');
 });
