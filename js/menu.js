53fcc3d27570b4a60657a0c24ef54e1ab07f9e54
Nelson Carvajal <nelcarca@gmail.com>
2021-12-10 22:28:05 -0300

    modificaciones celular vista

---
+++ js/menu.js
@@ -0,0 +1,8 @@
+let burgerButton = document.getElementById('burger-button');
+let menu = document.getElementById('menu');
+
+// burguerButton contiene el span con el icono
+// burguerButton.addEventListener('que evento', 'que deseo hacer');
+burgerButton.addEventListener('touchstart', function() {
+  menu.classList.toggle('active');
+});
